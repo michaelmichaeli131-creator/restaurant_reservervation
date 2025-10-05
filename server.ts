b2e72e918707ade1bbc1daed1e6c7ce6f8fa9f96
Michael Michaeli <michaelm@transchip.com>
2025-10-06 01:08:15 +0300

    Initial deploy

--- server.ts
+++ server.ts
@@ -33,7 +33,6 @@ import ownerCapacityRouter from "./routes/owner_capacity.ts";
 import { listRestaurants, getUserById } from "./database.ts";
 import { sendVerifyEmail } from "./lib/mail.ts";
 import ownerManageRouter from "./routes/owner_manage.ts";
-import { ownerDashboardRouter } from "./routes/owner_dashboard.ts";
 import { ownerHoursRouter } from "./routes/owner_hours.ts";
 
 
@@ -299,7 +298,6 @@ app.use(ownerManageRouter.allowedMethods());
 
 app.use(authRouter.routes());
 app.use(restaurantsRouter.routes());
-app.use(ownerDashboardRouter.routes());
 app.use(ownerHoursRouter.routes());
 
 // --- OPTIONS & 404 ---
